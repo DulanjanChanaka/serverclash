7f49bcbee8e0bf1692a6f0aab3f11e98584629e7
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-11-04 08:26:16 +0530

    order

--- routes/top-photo.js
+++ routes/top-photo.js
@@ -53,7 +53,7 @@ router.get("/find/:id", async (req, res) => {
 //GET ALL USER
 router.get("/", async (req, res) => {
   try {
-    const data = await TopPhoto.find().sort("description");
+    const data = await TopPhoto.find().sort({ description: -1 });
     res.status(200).json(data);
   } catch (err) {
     res.status(500).json(err);
